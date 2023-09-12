d9ea92c60d76ef56ff3d93e37751ca3d913df81a
Warlord-K <warlordk28@gmail.com>
2023-09-12 16:50:10 +0530

    Added Refiner Arguements to SDXL

--- segmind/SDXL.py
+++ segmind/SDXL.py
@@ -1,5 +1,25 @@
 from .T2I import T2I
+import random
+import requests
+from io import BytesIO
+from PIL import Image
 
+"""
+"style": "base",
+  "samples": 1,
+  "scheduler": "UniPC",
+  "num_inference_steps": 25,
+  "guidance_scale": 8,
+  "strength": 0.2,
+  "seed": 468685,
+  "img_width": 1024,
+  "img_height": 1024,
+  "refiner": true,
+  "high_noise_fraction": 0.8,
+  "base64": false
+
+
+"""
 
 class SDXL(T2I):
     def __init__(self, api_key=None):
@@ -12,19 +32,38 @@ class SDXL(T2I):
         prompt: str,
         negative_prompt: str = "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, blurry, bad anatomy, blurred, watermark, grainy, signature, cut off, draft",
         samples=1,
-        scheduler="dpmmp_sde_ancestral",
+        style="base",
+        scheduler="UniPC",
         num_inference_steps=25,
-        guidance_scale=7,
+        guidance_scale=8,
         seed=None,
-        strength=0.75,
+        strength=0.2,
+        refiner=True,
+        high_noise_fraction=0.8,
+        base64=False,
     ):
-        return super().generate(
-            prompt=prompt,
-            negative_prompt=negative_prompt,
-            samples=samples,
-            scheduler=scheduler,
-            num_inference_steps=num_inference_steps,
-            guidance_scale=guidance_scale,
-            seed=seed,
-            strength=strength,
-        )
+        if not seed:
+            seed = random.randint(0, 1000000000)
+        data = {
+            "prompt": prompt,
+            "negative_prompt": negative_prompt,
+            "samples": f"{samples}",
+            "scheduler": scheduler,
+            "img_height": 1024,
+            "img_width": 1024,
+            "num_inference_steps": f"{num_inference_steps}",
+            "guidance_scale": f"{guidance_scale}",
+            "seed": f"{seed}",
+            "strength": f"{strength}",
+            "refiner": refiner,
+            "high_noise_fraction": f"{high_noise_fraction}",
+            "base64": base64,
+        }
+        response = requests.post(self.url, json=data, headers=self.headers)
+        if response.status_code == 200:
+            print(
+                f"Success! You have {response.headers['X-remaining-credits']} credits remaining"
+            )
+            return Image.open(BytesIO(response.content))
+        else:
+            raise Exception(f"Error: {response.status_code}")
